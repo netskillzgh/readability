69683015ae6799cae7cf9633ad133866651f8855
Rollo <91226658+netskillzgh@users.noreply.github.com>
2024-08-18 14:37:15 +0200

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,6 @@ use url;
 
 #[derive(Debug)]
 pub enum Error {
-    #[cfg(feature = "reqwest")]
     NetworkError(reqwest::Error),
     UrlParseError(url::ParseError),
     Unexpected,
@@ -14,7 +13,6 @@ pub enum Error {
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
         match *self {
-            #[cfg(feature = "reqwest")]
             Error::NetworkError(ref e) => write!(f, "NetworkError:  {}", e),
             Error::UrlParseError(ref e) => write!(f, "UrlParseError:  {}", e),
             Error::Unexpected => write!(f, "UnexpectedError"),
@@ -27,8 +25,6 @@ impl From<url::ParseError> for Error {
         Error::UrlParseError(err)
     }
 }
-
-#[cfg(feature = "reqwest")]
 impl From<reqwest::Error> for Error {
     fn from(err: reqwest::Error) -> Error {
         Error::NetworkError(err)
